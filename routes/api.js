5d743bc307d03181be6630ac11849fc493c6d4be
PujithaKoneru <pkoneru@horizon.csueastbay.edu>
2020-12-18 17:13:38 -0500

    Final Commit

---
+++ routes/api.js
@@ -0,0 +1,30 @@
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const router = vertex.router()
+
+const Profile = require('../models/Profile')
+
+router.get('/profile', (req, res) => {
+	let filters = req.query
+	if (req.query.age != null){
+		filters = {
+			age: {$gt: req.query.age}
+		}
+	}
+
+	Profile.find(filters)
+	.then(profiles => {
+		res.json({
+			confirmation: 'success',
+			data: profiles
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+})
+
+module.exports = router
